9b450400c2eadf0140069867c2a74e71a4c24b1a
athuria <110605779+athuria@users.noreply.github.com>
2022-10-07 13:03:32 +0700

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -3,4 +3,4 @@ game:GetService("Players").LocalPlayer.Idled:Connect()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
    wait(1)
    vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
-end)
+
